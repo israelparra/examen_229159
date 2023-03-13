a7bf0b7bb14a6ec0de104dd9a3f5f2c75a5f9c46
israelparra <israelparracamara@gmail.com>
2023-03-13 12:54:40 -0700

    e

---
+++ app/src/main/java/mx/edu/itson/potros/examenu2/Detalles.kt
@@ -0,0 +1,9 @@
+package mx.edu.itson.potros.examenu2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Detalles( var image:Int,
+                var titulo:String,
+                var precio:String
+                )
---
+++ app/src/main/java/mx/edu/itson/potros/examenu2/MainActivity.kt
@@ -0,0 +1,21 @@
+package mx.edu.itson.potros.examenu2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var button: Button = findViewById(R.id.btn_visitar)
+
+        button.setOnClickListener{
+            var intento = Intent(this,Catalogo::class.java)
+            this.startActivity(intento)
+        }
+    }
+}
